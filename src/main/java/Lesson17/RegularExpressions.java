6ad73b7e59ff518f1f1a16081278213dee9a59ad
EvgeniYunevich1 <yunevich_es@mail.ru>
2020-04-16 19:52:29 +0300

    Lesson17

--- src/main/java/Lesson17/RegularExpressions.java
+++ src/main/java/Lesson17/RegularExpressions.java
@@ -4,12 +4,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegularExpressions {
-//Провалидировать адрес электронного  кошелька, пример начало всегда с букв 'E' 'D' 'R' после буквы следает 12 чисел,
+    //Провалидировать адрес электронного  кошелька, пример начало всегда с букв 'E' 'D' 'R' после буквы следает 12 чисел,
 // возможен ввод с пробелами это не должно влять на нашу проверку
-    private static final String zipPattern ="^\\s*?[EDR]\\s*\\d\\s*\\d\\s*\\d\\s*\\d\\s*\\d\\s*\\d\\s*\\d\\s*\\d\\s*\\d\\s*\\d\\s*\\d\\s*\\d\\s*$";
+    private static final String zipPattern = "^\\s*[EDR]\\s*([0-9]\\s*){12}\\s*$";
+
     public static void main(String[] args) {
-       Pattern pattern=Pattern.compile(zipPattern);
-        Matcher matcher=pattern.matcher(" D1234567 89012");
+        Pattern pattern = Pattern.compile(zipPattern);
+        Matcher matcher = pattern.matcher(" D                        1 2 3 456 78 90 12 ");
         System.out.println(matcher.matches());
     }
 }
